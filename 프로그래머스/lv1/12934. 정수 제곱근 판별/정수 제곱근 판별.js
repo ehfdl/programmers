48e0d6a2d018e1c9bd25dce2012bd4fb2a1dfcd6
ehfdl <117057669+ehfdl@users.noreply.github.com>
2023-03-28 10:26:56 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var num = Math.sqrt(n);
+    let answer = 0;
+    if(num % 1 === 0){
+        answer = (num + 1)**2
+    }
+    else{
+        answer = -1
+    }
+    return answer
+}
