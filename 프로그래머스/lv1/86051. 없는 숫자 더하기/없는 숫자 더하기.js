d59aec568d90298f62dbc6715f1b2be30d8949c9
ehfdl <117057669+ehfdl@users.noreply.github.com>
2023-04-05 11:33:43 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -0,0 +1,10 @@
+function solution(numbers) {
+    let answer = 0;
+    for(i=0; i<=9; i++){
+        if(!numbers.includes(i)){
+            answer += i
+            
+        }
+}
+    return answer;
+}
