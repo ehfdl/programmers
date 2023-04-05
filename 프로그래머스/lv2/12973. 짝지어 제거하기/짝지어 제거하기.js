e1b12a4fa9c5c404c050ba19c1f47d2568268e76
ehfdl <117057669+ehfdl@users.noreply.github.com>
2023-04-05 14:50:11 +0900

    [level 2] Title: 짝지어 제거하기, Time: 38.05 ms, Memory: 58.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,13 @@
+function solution(s)
+{
+    let answer = []
+    for(let i of s){
+        answer.push(i)
+        if(answer[answer.length-1] === answer[answer.length-2]){
+            answer.pop();
+            answer.pop();
+        }
+    }
+
+    return answer.length === 0 ? 1 : 0;
+}
