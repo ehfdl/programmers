25167d007e354eb5c5695d4798b09ff4a91e5f45
ehfdl <117057669+ehfdl@users.noreply.github.com>
2023-04-06 10:39:23 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,16 +1,9 @@
 function solution(n)
 {
-    var ans = 1;
+    var ans = 0;
 
-    while(n !== 1){
-        if(n%2===0){
-            n = n/2
-        }
-        else{
-            n=n-1
-            ans++
-            }
-    }
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    console.log('Hello Javascript')
 
     return ans;
 }
