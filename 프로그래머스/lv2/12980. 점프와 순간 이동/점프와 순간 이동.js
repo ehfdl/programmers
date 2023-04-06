1eacee1e264514f793beede1ff77605486ae8b27
ehfdl <117057669+ehfdl@users.noreply.github.com>
2023-04-06 10:38:56 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,16 @@
+function solution(n)
+{
+    var ans = 1;
+
+    while(n !== 1){
+        if(n%2===0){
+            n = n/2
+        }
+        else{
+            n=n-1
+            ans++
+            }
+    }
+
+    return ans;
+}
