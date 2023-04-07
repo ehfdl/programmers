eaf0686786af9e2e1322962f31d0e71d730239b6
ehfdl <117057669+ehfdl@users.noreply.github.com>
2023-04-07 11:25:58 +0900

    [level 1] Title: 내적, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,7 +1,7 @@
 function solution(a, b) {
-    let answer = 0;
-    for(i=0; i<a.length; i++){
-        answer += a[i] * b[i]
-    }
-    return answer;
+    // let answer = 0;
+    // for(i=0; i<a.length; i++){
+    //     answer += a[i] * b[i]
+    // }
+    return a.reduce((x,_,i) => x += a[i] * b[i], 0);
 }
